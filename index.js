c04b713189705a473cb1e4f13fa07527e0503316
hoefl <50425444+hoefl@users.noreply.github.com>
2019-05-15 14:38:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(server);
 var players = [];
 
 
-server.listen(80, function(){
+server.listen(process.env.PORT || 8080, function(){
 	console.log("Server is now running.........");
 });
 
